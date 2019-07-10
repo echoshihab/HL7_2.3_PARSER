0e61d5c9087c4e554e6eff43d67e421a72f425fe
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2019-07-10 15:46:16 -0400

    output parsed file as text

--- coreParser.py
+++ coreParser.py
@@ -1,46 +1,22 @@
 
-from datetime import datetime
+from seg_parsers import MSHparser, PIDparser, PV1parser, ORCparser, OBRparser, OBX1parser
+
+textFile = open("parsedFile.txt", "wt")
 
 with open('lorem.txt', 'rt') as myfile:
     for line in myfile:
         if line.startswith('MSH'):
-            MSH = line.split('|')
-            print(f'HL7 Version: {MSH[-2]}')
+            MSHparser(line, textFile)
         if line.startswith('PID'):
-            PID = line.split('|')
-            pt_name = PID[5].replace('^', ' ')
-            pt_dob = datetime.strptime(PID[7], '%Y%m%d').strftime('%d %b %Y')
-            pt_gender = PID[8]
-            pt_address = PID[11].replace('^', ' ')
-            print(f'Patient Name: {pt_name}')
-            print(f'DOB: {pt_dob}')
-            print(f'Address: {pt_address}')
+            PIDparser(line, textFile)
         if line.startswith('PV1'):
-            PV1 = line.split('|')
-            location_list = PV1[3].split('^')
-            physician_list = PV1[8].split('^')
-            exam_location = location_list[3]
-            ref_physician = physician_list[2] + ' ' + physician_list[1]
-            print(f'Exam Location: {exam_location}')
-            print(f'Referring Physician: {ref_physician}')
+            PV1parser(line, textFile)
         if line.startswith('ORC'):
-            ORC = line.split('|')
-            exam_date = datetime.strptime(
-                ORC[7][0:8], '%Y%m%d').strftime('%d %b %Y')
-            print(f'Exam Date: {exam_date}')
+            ORCparser(line, textFile)
         if line.startswith('OBR'):
-            OBR = line.split('|')
-            accession = OBR[2]
-            procedure_info = OBR[4].split('^')
-            procedure_code = procedure_info[0]
-            procedure_desc = procedure_info[1]
-            reading_rad = OBR[-6].split('^')
-            print(f'Reading Radiologist: {reading_rad[2]} {reading_rad[1]}')
-            print(f'Procedure code: {procedure_code}  Procedure Description: {procedure_desc}')
+            OBRparser(line, textFile)
         if line.startswith('OBX|1'):
-            OBX1 = line.split('|')
-            report_body = OBX1[5].replace('\\.br\\', ' ')
-            print(f'Report: {report_body}')
+            OBX1parser(line, textFile)
 
-   # new_line = myline.replace("/", "").replace("*", "")
-        #print(new_line, end="")
+       # new_line = myline.replace("/", "").replace("*", "")
+            # print(new_line, end="")
